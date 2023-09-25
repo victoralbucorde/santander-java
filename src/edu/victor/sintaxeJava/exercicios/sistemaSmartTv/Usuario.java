5f5eadf59a98b3137f58c24da6fd17b489b1a7f7
victoralbucorde <victoralbucorde@gmail.com>
2023-09-25 10:33:29 -0300

    Exercicio smartTv mudanca

--- src/edu/victor/sintaxeJava/exercicios/sistemaSmartTv/Usuario.java
+++ src/edu/victor/sintaxeJava/exercicios/sistemaSmartTv/Usuario.java
@@ -1,11 +1,14 @@
 package edu.victor.sintaxeJava.exercicios.sistemaSmartTv;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Usuario {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         SmartTv smartTv = new SmartTv();
 
-        System.out.println(smartTv.canal);
-        System.out.println(smartTv.volume);
-        System.out.println(smartTv.ligadoDesligado);
+        smartTv.canal = scanner.nextInt();
+
     }
 }
