1a8600d28eae9df4f2e7b24d2a31f9b8aea16fa3
victoralbucorde <victoralbucorde@gmail.com>
2023-10-09 14:33:13 -0300

    Add CarrinhoDeCompras exercicio

---
+++ src/edu/victor/collectionsJava/exercicios/carrinhoDeCompras/CarrinhoDeCompras.java
@@ -0,0 +1,58 @@
+package edu.victor.collectionsJava.exercicios.carrinhoDeCompras;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+    private List<Item> listaDeCompras;
+
+    public CarrinhoDeCompras(List<Item> listaDeCompras) {
+        this.listaDeCompras = new ArrayList<>();
+    }
+
+    public void adicionarItem(String nome, double preco, int quantidade){
+        listaDeCompras.add(new Item(nome,preco,quantidade));
+    }
+
+    public void removerItem(String nome){
+        List<Item> listaParaRemover = new ArrayList<>();
+
+        if(!listaDeCompras.isEmpty()){
+            for(Item i : listaDeCompras){
+                if(i.getNome().equalsIgnoreCase(nome)){
+                    listaParaRemover.add(i);
+                }
+            }
+            listaDeCompras.removeAll(listaParaRemover);
+        }else{
+            System.out.println("Lista está vazia!");
+        }
+    }
+
+    public double calcularValorTotal() {
+        double sum = 0;
+        if (!listaDeCompras.isEmpty()){
+            for (Item i : listaDeCompras){
+                sum += i.getPreco() * i.getQuantidade();
+            }
+            return sum;
+        }else{
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "CarrinhoDeCompras{" +
+                "listaDeCompras=" + listaDeCompras +
+                '}';
+    }
+
+    public void exibirItens(){
+        if (!listaDeCompras.isEmpty()){
+            System.out.println(listaDeCompras);
+        }else{
+            System.out.println("A lista está vazia");
+        }
+    }
+}
